b1a7095020c08efd7fa64ac45a01e139016dfffc
Georg Lukas <georg@op-co.de>
2019-03-07 11:08:27 +0100

    MUC: on enter(), check both the domain and the room for MUC feature
    Some MUCs are deployed on a domain that's not actually a MUC service
    domain (I'm looking at you, chat@dino.im). This code additionally checks
    the room JID for the `http://jabber.org/protocol/muc` feature.
    
    Signed-off-by: Georg Lukas <georg@op-co.de>

--- smack-extensions/src/main/java/org/jivesoftware/smackx/muc/MultiUserChatManager.java
+++ smack-extensions/src/main/java/org/jivesoftware/smackx/muc/MultiUserChatManager.java
@@ -56,6 +56,7 @@ import org.jivesoftware.smackx.muc.MultiUserChatException.NotAMucServiceExceptio
 import org.jivesoftware.smackx.muc.packet.MUCInitialPresence;
 import org.jivesoftware.smackx.muc.packet.MUCUser;
 
+import org.jxmpp.jid.BareJid;
 import org.jxmpp.jid.DomainBareJid;
 import org.jxmpp.jid.EntityBareJid;
 import org.jxmpp.jid.EntityJid;
@@ -378,7 +379,7 @@ public final class MultiUserChatManager extends Manager {
      * @see <a href="http://xmpp.org/extensions/xep-0045.html#disco-service-features">XEP-45 § 6.2 Discovering the Features Supported by a MUC Service</a>
      * @since 4.2
      */
-    public boolean providesMucService(DomainBareJid domainBareJid) throws NoResponseException,
+    public boolean providesMucService(BareJid domainBareJid) throws NoResponseException,
                     XMPPErrorException, NotConnectedException, InterruptedException {
         return ServiceDiscoveryManager.getInstanceFor(connection()).supportsFeature(domainBareJid,
                         MUCInitialPresence.NAMESPACE);
